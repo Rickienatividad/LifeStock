7ca2f6ddc0eac35dd68a6c49305619e7ccd3b007
CambyCult <84335427+CambyCult@users.noreply.github.com>
2023-04-15 20:27:29 -0400

    initial commit for lifestock rebuild.

---
+++ lifestock/src/App.js
@@ -0,0 +1,14 @@
+
+import './style/App.css';
+import { Toolbar } from './components/toolbar.js'
+import './style/toolbar.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Toolbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ lifestock/src/components/toolbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../style/toolbar.css';
+
+export const Toolbar = () => (
+    <div className="toolbar">
+        TOOLBAR
+    </div>
+
+)
