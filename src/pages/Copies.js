882da43b44ff02666498d0f1ba2bca1b7a87f50b
Rickie Natividad <rickienatividad85@gmail.com>
2023-05-21 12:57:48 -0400

    copies of checklists are rendered to /copies . Not dynamically rendered within modal yet

--- src/pages/Copies.js
+++ src/pages/Copies.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Fragment } from "react";
 import "../style/Copies.css";
+import { Link } from "react-router-dom";
+import { Modal } from "../components/Modal";
 
 export function Copies() {
   // acquire rig checklist index for all rigs
@@ -31,40 +33,59 @@ export function Copies() {
     return item;
   });
 
+  const [isListVisible, setIsListVisible] = useState(false);
+  const [currentList, setCurrentList] = useState({});
+
+  const showList = (list) => {
+    setIsListVisible(true);
+    setCurrentList(list);
+  };
+
+  const hideList = () => {
+    setIsListVisible(false);
+  };
+
   return (
     <div className="main">
       <div>
-        {listArray.map((list) => (
-          <table key={list.id}>
-            <thead>
-              <tr>
-                <td>Item</td>
-                <td>Minimum Count</td>
-                <td>Actual Count</td>
-                <td>Rig Id: {list.rig_id}</td>
-              </tr>
-            </thead>
-            {array.map((manifest) =>
-              manifest.checklist_id === list.id ? (
-                <tbody key={Math.random(manifest.id)}>
+        {listArray
+          .sort((a, b) => a.rig_id - b.rig_id)
+          .map((list) => (
+            <div key={list.id}>
+              <Link to="#" onClick={showList}>
+                Rig: {list.rig_id} - {list.date}
+              </Link>
+              <table style={{ display: isListVisible ? "block" : "none" }}>
+                <thead>
                   <tr>
-                    <td>{manifest.item}</td>
-                    <td>{manifest.minimum}</td>
-                    <td>{manifest.actual}</td>
+                    <td>Item</td>
+                    <td>Minimum Count</td>
+                    <td>Actual Count</td>
+                    <td>Rig Id: {list.rig_id}</td>
+                  </tr>
+                </thead>
+                {array.map((manifest) =>
+                  manifest.checklist_id === list.id ? (
+                    <tbody key={Math.random(manifest.id)}>
+                      <tr>
+                        <td>{manifest.item}</td>
+                        <td>{manifest.minimum}</td>
+                        <td>{manifest.actual}</td>
+                      </tr>
+                    </tbody>
+                  ) : (
+                    // <></>
+                    <Fragment key={Math.random()} />
+                  )
+                )}
+                <tbody>
+                  <tr>
+                    <td>Rig {list.rig_id}</td>
                   </tr>
                 </tbody>
-              ) : (
-                // <></>
-                <Fragment key={Math.random()} />
-              )
-            )}
-            <tbody>
-              <tr>
-                <td>Rig {list.rig_id}</td>
-              </tr>
-            </tbody>
-          </table>
-        ))}
+              </table>
+            </div>
+          ))}
       </div>
     </div>
   );
